a1198d9de56587578587a532a4c63225db7025bf
Pvrolomx <pvrolo@gmail.com>
2026-02-02 08:59:16 -0600

    [PWA] Add service worker

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'kin-maya-v1';
+const urlsToCache = ['/', '/manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter(c => c !== CACHE_NAME).map(c => caches.delete(c))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
